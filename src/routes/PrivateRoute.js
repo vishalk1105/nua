af883feceabb97d0ebdf008bdb26b4c88541b8e3
vishal vilas kamble <vishalvkamble95@gmail.com>
2024-05-23 23:01:15 +0530

    myprofile

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,7 +2,6 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
   const auth = localStorage.getItem("token");
-  console.log(auth, "auth");
   if (auth === undefined) {
     <Navigate to="/login" />;
   }
--- src/view/MyProfile.js
+++ src/view/MyProfile.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import MainLayout from "../layout/MainLayout";
+import { Link } from "react-router-dom";
 const MyProfile = () => {
   return (
     <MainLayout>
@@ -9,21 +10,72 @@ const MyProfile = () => {
           <div className="col-md-7">
             <div className="card p-3 py-4">
               <div className="text-center">
-                <img src={""} width="100" className="rounded-circle" alt="" />
+                <img
+                  src={
+                    "https://avatars.githubusercontent.com/u/120122471?s=400&u=4cd75eeb79e65a9df8990d5223411d328b0791cf&v=4"
+                  }
+                  width="200"
+                  className="rounded-circle"
+                  alt=""
+                />
               </div>
 
               <div className="text-center mt-3">
-                <span className="bg-secondary p-1 px-4 rounded text-white"></span>
-                <span></span>
-
                 <div className="px-4 mt-1">
-                  <p className="fonts">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris
+                  <p className="fonts text-justify navTitle">
+                    Hey there! I'm Vishal Kamble, and I've been diving into
+                    frontend development for a year now, rocking the MERN stack
+                    like a champ! But hey, that's not all – I've got a cool
+                    handle on Java and MySQL too. What keeps me going? Well, I
+                    thrive on learning new tech stuff, and I'm up for any tech
+                    adventure that comes my way!
                   </p>
                 </div>
+                <h3 className="projectHead">Side Projects</h3>
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td className="projectName">EPIC HIKES BACKEND</td>
+                      <td>
+                        <Link
+                          to={
+                            "https://github.com/vishalk1105/epic-hikes-backend-project"
+                          }
+                          target="_blank"
+                          className="projectLink"
+                        >
+                          Project
+                        </Link>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="projectName">EPIC HIKES FRONTEND</td>
+                      <td>
+                        <Link
+                          to={
+                            "https://github.com/vishalk1105/epic-hikes-frontend-project"
+                          }
+                          target="_blank"
+                          className="projectLink"
+                        >
+                          Project
+                        </Link>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="projectName">SWIGGY CLONE</td>
+                      <td>
+                        <Link
+                          to={"https://github.com/vishalk1105/swiggy-clone"}
+                          target="_blank"
+                          className="projectLink"
+                        >
+                          Project
+                        </Link>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
